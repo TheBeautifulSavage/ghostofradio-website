c5ee570d2fc3eba9f62c31fc71da966ff79d551b
Jesse Hull <mac1@Jesses-Mac-mini.local>
2026-03-31 22:09:16 -0700

    feat: PWA installable app

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'gor-v2';
+const STATIC = ['/', '/index.html', '/shows.html', '/css/style.css', '/js/main.js', '/images/logo.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
